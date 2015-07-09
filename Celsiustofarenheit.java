dc1d672ae29bd0991812ef05bc5c265ca1229f7d
jamesbuckijr <jamesbuckijr@gmail.com>
2015-07-08 21:34:48 -0400

    Introduction to Java Programming

--- Celsiustofarenheit.java
+++ Celsiustofarenheit.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Celsiustofarenheit {
     public static void main(String[] args) {
-            import java.util.Scanner;
-            Scanner input = new Scanner(System.in);
+        //create scanner for input from console    
+        Scanner input = new Scanner(System.in);
+        //Ask for user input
+        System.out.print("Enter the degrees in Celsius:");
+        //declare int celsius variable
+        int celsius = input.nextInt();
+        //declare double and convert to farenheit using (9.0 / 5 ) * celsius + 32
+        double farenheit = (9.0 / 5) * celsius + 32;
+        // return string with final result
+        System.out.println(celsius + " degress in Celsius is " + farenheit + " degrees in Farenheit.");
     }
 }
