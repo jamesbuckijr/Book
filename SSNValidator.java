afbaa61c19e3ffaed620bf39530fc479ced421f2
jamesbuckijr <jamesbuckijr@gmail.com>
2015-08-31 22:37:00 -0400

    String I/O

---
+++ SSNValidator.java
@@ -0,0 +1,14 @@
+import java.
+
+public class SSNValidator {
+    /** Tests whether a given SSN in format ###-##-#### is valid or not */
+    public static void main(String[] args) {
+        
+    }
+    public boolean validator() {}
+    public cycle_input() {
+    
+    }
+    public static getInput() {}
+    public static displayRun() {}
+}
